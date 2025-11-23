2a389498bfbeb281e5579f11c4261903c45d5869
Andreas Maurer <245673595+andreas-maurer-2006@users.noreply.github.com>
2025-11-23 14:21:56 +0100

    dev(day1): setting up file read

---
+++ 2015/source/javascript/day1.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const resource = "../../resource/";
+
+fs.readFile(resource+"input-day1.txt", "utf8", (error, data) => {
+	if (error)
+		throw error;
+	console.log(data);
+});
+
+function day1p1(input) {
+
+}
+
+function day1p2(input) {
+
+}
