d1327177785269660baf3fc091c5f738d516cbfb
Andreas Maurer <245673595+andreas-maurer-2006@users.noreply.github.com>
2025-11-23 14:39:49 +0100

    dev(day1): day1 completed

--- 2015/source/javascript/day1.js
+++ 2015/source/javascript/day1.js
@@ -1,16 +1,30 @@
 const fs = require("fs");
 const resource = "../../resource/";
 
-fs.readFile(resource+"input-day1.txt", "utf8", (error, data) => {
+fs.readFile(resource+"input-day1.txt", "utf8", (error, input) => {
 	if (error)
 		throw error;
-	console.log(data);
+	console.log(day1p1(input));
+	console.log(day1p2(input));
 });
 
-function day1p1(input) {
 
+function day1p1(input) {
+	let floor = 0;
+	for (let i = 0; i < input.length; ++i) {
+		floor += input[i] == '(';
+		floor -= input[i] == ')';
+	}
+	return floor;
 }
 
 function day1p2(input) {
-
+	let floor = 0;
+	for (let i = 0; i < input.length; ++i) {
+		floor += input[i] == '(';
+		floor -= input[i] == ')';
+		if (floor == -1)
+			return i+1;
+	}
+	return null;
 }
