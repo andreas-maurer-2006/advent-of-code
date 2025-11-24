df840ad6262d98c133a017dd6b0e90361ecff048
Andreas Maurer <245673595+andreas-maurer-2006@users.noreply.github.com>
2025-11-24 07:23:20 +0100

    dev(day2): day 2 part 2 completed

--- 2015/source/javascript/day2.js
+++ 2015/source/javascript/day2.js
@@ -5,6 +5,7 @@ fs.readFile(resource+"input-day2.txt", "utf8", (error, input) => {
 	if (error)
 		throw error;
 	console.log(day2p1(input));
+	console.log(day2p2(input));
 });
 
 function day2p1(input) {
@@ -20,5 +21,14 @@ function day2p1(input) {
 }
 
 function day2p2(input) {
-
+	const lines = input.split('\n');
+	let wrapping_paper = 0;
+	for (let i = 0; i < lines.length-1; ++i) {
+		const [l, w, h] = lines[i].split('x').map(Number);
+		const sorted_sides = [l, w, h].sort((x, y) => x - y);
+		wrapping_paper
+			+= l*w*h
+			+ 2 * sorted_sides[0] + 2 * sorted_sides[1];
+	}
+	return wrapping_paper;
 }
