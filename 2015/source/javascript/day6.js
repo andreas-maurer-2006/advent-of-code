81c7a6133e02dab39da19d5b6283031a19bd99ed
Andreas Maurer <245673595+andreas-maurer-2006@users.noreply.github.com>
2025-11-27 15:21:51 +0100

    dev(day6): day 6 part 1 completed

---
+++ 2015/source/javascript/day6.js
@@ -0,0 +1,62 @@
+const fs = require("fs")
+const resource = "../../resource/";
+
+
+fs.readFile(resource+"input-day6.txt", "utf8", (error, input) => {
+	if (error)
+		throw error;
+	console.log(day6p1(input));
+	console.log(day6p2(input));
+});
+
+function day6p1(input) {
+	const lights =
+		Array.from({ length: 1000 },
+		() => Array(1000).fill(false));
+	const commands = input.split(/\r?\n/);
+	let sum = 0;
+
+	for (let i = 0; i < commands.length-1; ++i) {
+		day6p1_command(commands[i], lights);
+	}
+
+	for (let i = 0; i < lights.length; ++i) {
+		for (let j = 0; j < lights[0].length; ++j) {
+			sum += lights[i][j];
+		}
+	}
+
+	return sum;
+}
+
+function day6p2(input) {
+
+}
+
+function day6p1_command(command, lights) {
+	const [x1, y1, x2, y2] = command.match(/\d+/g).map(Number);
+
+	switch(true) {
+	case command.startsWith("turn on"):
+		for (let i = x1; i <= x2; ++i) {
+			for (let j = y1; j <= y2; ++j) {
+				lights[i][j] = true;
+			}
+		}
+		break;
+	case command.startsWith("turn off"):
+		for (let i = x1; i <= x2; ++i) {
+			for (let j = y1; j <= y2; ++j) {
+				lights[i][j] = false;
+			}
+		}
+		break;
+	case command.startsWith("toggle"):
+		for (let i = x1; i <= x2; ++i) {
+			for (let j = y1; j <= y2; ++j) {
+				lights[i][j] = !lights[i][j];
+			}
+		}
+		break;
+	}
+}
