a8ae28e3964cdb5058f9337df264935fbb998637
Andreas Maurer <245673595+andreas-maurer-2006@users.noreply.github.com>
2025-11-27 07:53:16 +0100

    dev(day3): day 3 part 2 completed

--- 2015/source/javascript/day3.js
+++ 2015/source/javascript/day3.js
@@ -5,21 +5,17 @@ fs.readFile(resource+"input-day3.txt", "utf8", (error, input) => {
 	if (error)
 		throw error;
 	console.log(day3p1(input));
+	console.log(day3p2(input));
 });
 
 function day3p1(input) {
-	const [min_x, min_y, len_x, len_y] = d3p1_max_xy(input);
+	const [min_x, min_y, len_x, len_y] = day3p1_max_xy(input);
 	const houses =
 		Array.from({ length: len_x },
 		() => Array(len_y).fill(false));
 	let x = Math.abs(min_x), y = Math.abs(min_y);
 	let visited = 1;
 	houses[x][y] = true;
-
-	console.log(
-		"x: "+x+"\ny: "+y
-		+"\nmin_x: "+min_x+"\nmin_y: "+min_y
-		+"\nlen_x: "+len_x+"\nlen_y: "+len_y);
 
 	for (let i = 0; i < input.length; ++i) {
 		switch (input[i]) {
@@ -58,14 +54,78 @@ function day3p1(input) {
 }
 
 function day3p2(input) {
+	/*
+		We could make the array size dynamic by calculating santas minimum
+		and length and robots minimum and length. The way this works can
+		be found on day3p1_max_xy function, however making a rectangle of
+		500; 500 will do the trick as well.
+	*/
+	const houses =
+		Array.from({ length: 500 },
+		() => Array(500).fill(false));
+	let x = 250, y = 250;
+	let santa_x = x, santa_y = y;
+	let robot_x = x, robot_y = y;
+	let visited = 1;
+	houses[x][y] = true;
 
+	for (let i = 0; i < input.length; ++i) {
+		if (i % 2) {
+			x = santa_x;
+			y = santa_y;
+		} else {
+			x = robot_x;
+			y = robot_y;
+		}
+		switch (input[i]) {
+		case '^':
+			y++;
+			if (!houses[x][y]) {
+				houses[x][y] = true;
+				visited++;
+			}
+			break;
+		case '>':
+			x++;
+			if (!houses[x][y]) {
+				houses[x][y] = true;
+				visited++;
+			}
+			break;
+		case 'v':
+			y--;
+			if (!houses[x][y]) {
+				houses[x][y] = true;
+				visited++;
+			}
+			break;
+		case '<':
+			x--;
+			if (!houses[x][y]) {
+				houses[x][y] = true;
+				visited++;
+			}
+			break;
+		}
+		if (i % 2) {
+			santa_x = x;
+			santa_y = y;
+		} else {
+			robot_x = x;
+			robot_y = y;
+		}
+	}
+
+	return visited;
+	
+	return 67;
 }
 
 /* Day 3 part 1 */
 
 // I have to calculate the minimum as well
 
-function d3p1_max_xy(input) {
+function day3p1_max_xy(input) {
 	let x, y, min_x, min_y, max_x, max_y;
 	x = y = min_x = min_y = max_x = max_y = 0;
 	for (let i = 0; i < input.length; ++i) {
