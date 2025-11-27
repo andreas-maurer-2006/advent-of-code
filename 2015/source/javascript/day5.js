521f8cd9382c059efd32b7991ebe9073def50770
Andreas Maurer <245673595+andreas-maurer-2006@users.noreply.github.com>
2025-11-27 10:35:50 +0100

    dev(day5): day 5 part 2 completed

--- 2015/source/javascript/day5.js
+++ 2015/source/javascript/day5.js
@@ -18,7 +18,12 @@ function day5p1(input) {
 }
 
 function day5p2(input) {
-
+	let sum = 0;
+	const lines = input.split(/\r?\n/);
+	for (let i = 0; i < lines.length-1; ++i) {
+		sum += day5p2_is_string_nice(lines[i].trim());
+	}
+	return sum;
 }
 
 function day5p1_is_string_nice(str) {
@@ -38,4 +43,30 @@ function day5p1_is_string_nice(str) {
 	);
 
 	return vowels >= 3 && double_letter && no_illegal_strings;
+}
+
+function day5p2_is_string_nice(str) {
+	let pair_of_two_letter = false;
+	let repeat_with_letter_between = false;
+
+	for (let i = 1; i < str.length-1; ++i) {
+		if (str.split(str.substring(i-1, i+1)).length != 2) {
+			pair_of_two_letter = true;
+			/*console.log(
+				 "str.substring(i-1, i+1):"
+				+ str.substring(i-1, i+1)
+				+"str.split(str.substring(i-1, i+1)):"
+				+ str.split(str.substring(i-1, i+1)).length);*/
+		}
+		if (str[i-1] === str[i+1])
+			repeat_with_letter_between = true;
+	}
+
+	if (str.split(str.substring(str.length-2, str.length)).length != 2) {
+		pair_of_two_letter = true;
+		/*console.log("str.substring(str.length-2, str.length): "
+			+str.substring(str.length-2, str.length));*/
+	}
+
+	return pair_of_two_letter && repeat_with_letter_between;
 }
